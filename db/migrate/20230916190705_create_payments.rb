596deef8c6a24fab2d773c396aa3d07f139b96bd
0sugo <josugo26@gmail.com>
2023-09-16 23:56:24 +0300

    Add payment migrations

---
+++ db/migrate/20230916190705_create_payments.rb
@@ -0,0 +1,14 @@
+class CreatePayments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :payments do |t|
+      t.int :amount
+      t.string :name
+      t.references :order,null: false,foreign_key: true
+      t.datetime :date
+      t.string :payment_method
+      t.string :transaction_id
+
+      t.timestamps
+    end
+  end
+end
