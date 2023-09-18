11bfc40cd1b9e4942a6f41c10b37b59fbb7cccf7
0sugo <josugo26@gmail.com>
2023-09-19 01:12:30 +0300

    Add order model relationship

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  has_many :payments,dependent: :destroy
+  has_many :products, through: :orderproducts
+  has_many :orderproducts
+  belongs_to :staff_id
+  belongs_to :customer
 end
