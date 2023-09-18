7076c8d2e64e6ae357facc1d1ba2da437d2e82a2
0sugo <josugo26@gmail.com>
2023-09-18 23:23:58 +0300

    Updated staff migration

--- db/migrate/20230916190516_create_staffs.rb
+++ db/migrate/20230916190516_create_staffs.rb
@@ -3,7 +3,7 @@ class CreateStaffs < ActiveRecord::Migration[7.0]
     create_table :staffs do |t|
       t.string :name
       t.string :position
-      t.int :salary
+      t.integer :salary
 
       t.timestamps
     end
