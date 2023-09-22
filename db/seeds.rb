676d817feab02fe1c994b82f9c493a45f2234ab8
0sugo <josugo26@gmail.com>
2023-09-23 02:01:41 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,105 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# staff seeds
+Staff.create(
+  name:"Gladys",
+  position:"Waiter",
+  salary:6000
+)
+
+Staff.create(
+  name:"Emmah",
+  position:"Counter",
+  salary:7000
+)
+
+# products
+Product.create(
+  category:"Beer",
+  name:"Balozi",
+  price:230,
+)
+
+Product.create(
+  category:"Spirits",
+  name:"Kibao",
+  price:250,
+)
+
+#customer
+Customer.create(
+  name:"Muraya"
+)
+
+Customer.create(
+  name:"Mkisii"
+)
+
+# order
+Order.create(
+  receipt_number:"q2w3e4r",
+  customer_id:1,
+  order_date:Time.now,
+  staff_id:1
+)
+
+Order.create(
+  receipt_number:"q2w3e5r",
+  customer_id:2,
+  order_date:Time.now,
+  staff_id:2
+)
+
+#payment
+Payment.create(
+  amount:230,
+  name:"Muraya",
+  order_id:1,
+  date:Time.zone.parse("2023-04-04 11:30:00"),
+  payment_method:"CASH",
+  transaction_id:"CASH"
+)
+
+Payment.create(
+  amount:500,
+  name:"Mkisii",
+  order_id:2,
+  date:Time.zone.parse("2023-04-04 11:40:00"),
+  payment_method:"MPESA",
+  transaction_id:"RQERT3ERGH3"
+)
+
+#orderProduct
+product1 = Product.find(1)
+product2 = Product.find(2)
+
+Orderproduct.create(
+  order_id: 1,
+  product_id: 1,
+  quantity:2,
+  subtotal:product1.price*2
+)
+
+Orderproduct.create(
+  order_id: 1,
+  product_id: 2,
+  quantity:2,
+  subtotal:product2.price*2
+)
+
+Orderproduct.create(
+  order_id: 2,
+  product_id: 1,
+  quantity:2,
+  subtotal:product1.price*2
+)
+
+Orderproduct.create(
+  order_id: 2,
+  product_id: 2,
+  quantity:2,
+  subtotal:product2.price*2
+)
+
