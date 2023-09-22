32a7e653219b209e39569baefd7b5a98d4cebb93
0sugo <josugo26@gmail.com>
2023-09-23 01:59:20 +0300

    Add private function in orderproduct model to calculate subtotal

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   has_many :payments,dependent: :destroy
   has_many :products, through: :orderproducts
   has_many :orderproducts
-  belongs_to :staff_id
+
+  belongs_to :staff, class_name:'Staff',foreign_key:'staff_id'
   belongs_to :customer
 end
