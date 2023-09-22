eef673fa180f01c63c419c115874b40b7e73a973
0sugo <josugo26@gmail.com>
2023-09-23 02:00:21 +0300

    Modify orderproduct

--- app/models/orderproduct.rb
+++ app/models/orderproduct.rb
@@ -1,4 +1,12 @@
 class Orderproduct < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  before_save :calculateTotal
+
+  private
+
+  def calculateTotal
+    self.subtotal=product.price*quantity
+  end
 end
