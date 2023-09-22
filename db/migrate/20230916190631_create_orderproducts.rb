6d10211aaddb85008ef0f267c9c5e8d60fbfe566
0sugo <josugo26@gmail.com>
2023-09-23 01:55:57 +0300

    Modify orderproduct migration to have quantity and subtotal

--- db/migrate/20230916190631_create_orderproducts.rb
+++ db/migrate/20230916190631_create_orderproducts.rb
@@ -3,6 +3,8 @@ class CreateOrderproducts < ActiveRecord::Migration[7.0]
     create_table :orderproducts do |t|
       t.references :order, null: false, foreign_key: true, index: true
       t.references :product, null: false, foreign_key: true, index: true
+      t.integer :quantity
+      t.decimal :subtotal, precision: 8, scale: 2
 
       t.timestamps
     end
