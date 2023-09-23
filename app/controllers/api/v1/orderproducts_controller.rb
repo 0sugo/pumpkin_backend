8baf7788ae0edf2ef78827d9fbb23f1b327d407c
0sugo <josugo26@gmail.com>
2023-09-23 03:07:32 +0300

    Add index action to orderproducts controller

--- app/controllers/api/v1/orderproducts_controller.rb
+++ app/controllers/api/v1/orderproducts_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::OrderproductsController < ApplicationController
+  def index
+    @orderproducts = Orderproduct.all
+    render json: {status:"Success",message:"Order products successfully fetched",data:@orderproducts},status: :ok
+  end
 end
