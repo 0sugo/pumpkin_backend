c2b28b72144d5d88fc5b21d29bca27181a337f41
0sugo <josugo26@gmail.com>
2023-09-23 03:06:58 +0300

    Add index action to product controller

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::PaymentsController < ApplicationController
+ def index
+  @payments = Payment.all
+  render json: {status:"Success",message:"All payments fetched succesfully",data:@payments},status: :ok
+ end
 end
