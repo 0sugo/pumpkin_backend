34ca16f7d102e3e54b3994db645a07a2395f705c
0sugo <josugo26@gmail.com>
2023-09-23 03:07:19 +0300

    Add index action to customers controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::CustomersController < ApplicationController
+ def index
+  @customer = Customer.all
+  render json: { status:"Success", message:"All customers fetched sucessfully", data:@customer },status: :ok
+ end
 end
