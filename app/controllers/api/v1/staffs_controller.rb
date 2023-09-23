2c9713a8f2699f5879fcd38ce56e2303f559cb84
0sugo <josugo26@gmail.com>
2023-09-23 03:06:27 +0300

    Add index action to staff controller

--- app/controllers/api/v1/staffs_controller.rb
+++ app/controllers/api/v1/staffs_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::StaffsController < ApplicationController
+  def index
+    @staff = Staff.all
+    render json: { status:"Success",message:"All staff fetched succesfully",data:@staff },status: :ok
+   end
 end
