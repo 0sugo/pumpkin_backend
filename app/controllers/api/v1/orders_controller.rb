08710741428b1b966223e2ae640231604286ee13
0sugo <josugo26@gmail.com>
2023-09-25 22:00:47 +0300

    Add create method to orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,4 +3,17 @@ class Api::V1::OrdersController < ApplicationController
     @orders = Order.all
     render json: {status:"Success",message:"All orders fetched successfully",data:@orders},status: :ok
   end
+
+  def create
+    @order = Order.create(order_params)
+    if @order.save
+      render json: {status:'Sucesss',message:'Successfully saved order'},status: :ok
+    else
+      render json: {status:'Failed',message:'Failed to save order'},status: :unprocessable_entity
+    end
+  end
+
+  def order_params
+    params.require(:order).permit(:receipt_number,:customer_id,:order_date,:staff_id)
+  end
 end
