70f089c58a94bb7861b6cc67a743d0fb0977f44e
0sugo <josugo26@gmail.com>
2023-09-25 21:26:33 +0300

    Add  create method for customers

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::CustomersController < ApplicationController
   @customer = Customer.all
   render json: { status:"Success", message:"All customers fetched sucessfully", data:@customer },status: :ok
  end
+
+ def create
+  @customer = Customer.create(customer_params)
+
+  if @customer.save
+    render json: {status:'Success',message:'Customer created successfully'},status: :ok
+  else
+    render json: {status:'Failed',message:'Failed to create new customer'},status: :unprocessable_entity
+  end
+ end
+
+ private
+
+ def customer_params
+  params.require(:customer).permit(:name)
+ end
 end
