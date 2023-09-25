bacfdbf15f93056568b8090e62d2d9b11b72029d
0sugo <josugo26@gmail.com>
2023-09-25 21:15:35 +0300

    Add create method for staff

--- app/controllers/api/v1/staffs_controller.rb
+++ app/controllers/api/v1/staffs_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::StaffsController < ApplicationController
     @staff = Staff.all
     render json: { status:"Success",message:"All staff fetched succesfully",data:@staff },status: :ok
    end
+
+   def create
+    @staff = Staff.new(staff_params)
+
+    if @staff.save
+      render json: {status:'Success',message:'Staff created successfully'},status: :ok
+    else
+      render json: {status:'Failed',message:'Failed to create new staff'},status: :unprocessable_entity
+    end
+   end
+
+   private
+
+   def staff_params
+    params.require(:staff).permit(:name,:position,:salary)
+   end
 end
