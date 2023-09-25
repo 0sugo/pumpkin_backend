7ffbe2f2dfe00c54931dae548fcd24494f64e3ad
0sugo <josugo26@gmail.com>
2023-09-25 21:41:07 +0300

    Add create method to products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::ProductsController < ApplicationController
     @products = Product.all
     render json: {status:"Success",message:"All products fetched succesfully",data:@products},status: :ok
    end
+
+   def create
+    @product = Product.create(product_params)
+
+    if @product.save
+      render json: {status:'Success',message:'Product successfully created'},status: :ok
+    else
+      render json: {status:'Failed',message:'Failed to create new product'},status: :unprocessable_entity
+    end
+
+   end
+
+   private
+
+   def product_params
+    params.require(:product).permit(:category,:name,:price)
+   end
 end
