7ea2fbdf022231a495187db26b0dfc055d670c2f
0sugo <josugo26@gmail.com>
2023-09-26 12:36:31 +0300

    Add create method for payments

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::PaymentsController < ApplicationController
   @payments = Payment.all
   render json: {status:"Success",message:"All payments fetched succesfully",data:@payments},status: :ok
  end
+
+ def create
+  @payment = Payment.create(payment_params)
+
+  if @payment.save
+    render json: {status:'Success',message:'Payment successfully saved'},status: :ok
+  else
+    render json: {status:'Failed',message:'Failed to save new payment'},status: :unprocessable_entity
+  end
+
+ end
+
+ private
+
+ def payment_params
+  params.require(:payment).permit(:amount,:name,:order_id,:date,:payment_method,:transaction_id)
+ end
 end
