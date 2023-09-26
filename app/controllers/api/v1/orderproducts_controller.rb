f828e6184ff1322687bcba92a05a3c2d661962bf
0sugo <josugo26@gmail.com>
2023-09-26 16:18:07 +0300

    Add update quantity method after order has been made

--- app/controllers/api/v1/orderproducts_controller.rb
+++ app/controllers/api/v1/orderproducts_controller.rb
@@ -8,6 +8,9 @@ class Api::V1::OrderproductsController < ApplicationController
     @orderproduct = Orderproduct.create(orderproduct_params)
 
     if @orderproduct.save
+      product = Product.find(orderproduct_params[:product_id])
+      remaining_quantity = product.quantity - orderproduct_params[:quantity].to_i
+      product.update(quantity:remaining_quantity)
       render json: {status:'Success',message:'orderproduct created succesfully'},status: :ok
     else
       render json: {status:'Failed',message:'Failed to create orderproduct'},status: :unprocessable_entity
