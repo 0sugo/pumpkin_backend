236174dd055a4385e842ca35022a3e8b13d32b64
0sugo <josugo26@gmail.com>
2023-09-26 12:16:14 +0300

    Add create method to orderproduct

--- app/controllers/api/v1/orderproducts_controller.rb
+++ app/controllers/api/v1/orderproducts_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::OrderproductsController < ApplicationController
     @orderproducts = Orderproduct.all
     render json: {status:"Success",message:"Order products successfully fetched",data:@orderproducts},status: :ok
   end
+
+  def create
+    @orderproduct = Orderproduct.create(orderproduct_params)
+
+    if @orderproduct.save
+      render json: {status:'Success',message:'orderproduct created succesfully'},status: :ok
+    else
+      render json: {status:'Failed',message:'Failed to create orderproduct'},status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def orderproduct_params
+    params.require(:orderproduct).permit(:order_id,:product_id,:quantity,:subtotal)
+  end
 end
